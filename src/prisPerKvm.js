9b5069cb5c8abc3adf08598463c4f388ea5630b4
Tanet Trimas <tanet.trimas@tripletex.no>
2021-06-11 11:59:24 +0200

    Add missing permissions for external web calls

--- src/prisPerKvm.js
+++ src/prisPerKvm.js
@@ -1,81 +1,97 @@
-import setPricesInDOM from './helpers/dom/setPricesInDOM'
-import setTransportButton from './helpers/dom/setTransportButton'
-import getPricePerSquareMeter from './getPricePerSquareMeter'
-import { getItemFromStorage, setItemInStorage } from './localStorage'
-// setItemInStorage('tester', {key: 'value'})
-// if (getItemFromStorage('tester') !== null) {
-//   console.log('VALUE IS THERE!!!!!')
-// }
+import setPricesInDOM from "./helpers/dom/setPricesInDOM";
+import setTransportButton from "./helpers/dom/setTransportButton";
+import getPricePerSquareMeter from "./getPricePerSquareMeter";
+import findTrips from "./services/entur";
+import { getItemFromStorage, setItemInStorage } from "./localStorage";
 
-const adList = document.querySelector('.ads--list')
+const adList = document.querySelector(".ads--list");
 const RENDER_CHECK = 10;
 // Observer for catching DOM changes
-const mutationObserver = new MutationObserver(mutations => {
-  let removedCount = 0
+const mutationObserver = new MutationObserver((mutations) => {
+  let removedCount = 0;
   let addedCount = 0;
   mutations.forEach((mutation) => {
     if (mutation.removedNodes.length) {
-      ++removedCount
+      ++removedCount;
     }
 
     if (mutation.addedNodes.length) {
-      ++addedCount
+      ++addedCount;
     }
   });
   // Pageturn or filter changes will re-render the main function if more than 10 items are removed or added
-    if (removedCount > RENDER_CHECK || addedCount > RENDER_CHECK) {
-      main()
-    }
+  if (removedCount > RENDER_CHECK || addedCount > RENDER_CHECK) {
+    main();
+  }
 });
 
-  function main() {
-    const housingCardList = document.querySelectorAll('article > .ads__unit__content')
-    for (const housingCard of housingCardList) {
-      try {
-        const alreadyAppended = housingCard.querySelector('.priceSqm')
+function main() {
+  const housingCardList = document.querySelectorAll(
+    "article > .ads__unit__content"
+  );
+  for (const housingCard of housingCardList) {
+    try {
+      const alreadyAppended = housingCard.querySelector(".priceSqm");
 
-        if (alreadyAppended) {
-          continue;
-        }
-
-        const contentKeys = housingCard.querySelector('.ads__unit__content__keys')
-        const [squareMeterElement, priceElement] = [...contentKeys.childNodes]
-        const isAdCampaign = !squareMeterElement || !priceElement
-    
-        if (isAdCampaign) {
-          // Ad without price
-          continue
-        }
-
-        const priceStats = [...housingCard.querySelector('div > .u-float-left').children]
-        const totalPriceDiv = priceStats.find(e => e.textContent && e.textContent.toLowerCase().includes('totalpris'))
-        const priceList = priceElement.textContent.replace('kr', '').trim().split(/\s/)
-        // If the card does not have totalprice at all in the element, just use the prisantydning element
-        const basePrice = !!totalPriceDiv && totalPriceDiv.textContent.length 
-          ? totalPriceDiv.textContent.split('kr')[0] 
-          : priceList      
-        
-        if (priceList[0].toLowerCase() === 'solgt') {
-          // Drop to calculate if the property is sold
-          continue
-        }
-
-        // When there is a square metre range, use the highest value as factor
-        const pricePerSquareMetre = getPricePerSquareMeter(squareMeterElement, basePrice, priceList);
-
-        setPricesInDOM(pricePerSquareMetre, housingCard)
-        // const aTestDiv = document.createElement('div')
-        // aTestDiv.classList.add('mySimpleTest')
-        // adList.append(aTestDiv)
-
-        setTransportButton(housingCard)
-      } catch (error) { 
-        console.error(error)
-        break;
+      if (alreadyAppended) {
+        continue;
       }
+
+      const contentKeys = housingCard.querySelector(
+        ".ads__unit__content__keys"
+      );
+      const [squareMeterElement, priceElement] = [...contentKeys.childNodes];
+      const isAdCampaign = !squareMeterElement || !priceElement;
+
+      if (isAdCampaign) {
+        // Ad without price
+        continue;
+      }
+
+      const priceStats = [
+        ...housingCard.querySelector("div > .u-float-left").children,
+      ];
+      const totalPriceDiv = priceStats.find(
+        (e) =>
+          e.textContent && e.textContent.toLowerCase().includes("totalpris")
+      );
+      const priceList = priceElement.textContent
+        .replace("kr", "")
+        .trim()
+        .split(/\s/);
+      // If the card does not have totalprice at all in the element, just use the prisantydning element
+      const basePrice =
+        !!totalPriceDiv && totalPriceDiv.textContent.length
+          ? totalPriceDiv.textContent.split("kr")[0]
+          : priceList;
+
+      if (priceList[0].toLowerCase() === "solgt") {
+        // Drop to calculate if the property is sold
+        continue;
+      }
+
+      // When there is a square metre range, use the highest value as factor
+      const pricePerSquareMetre = getPricePerSquareMeter(
+        squareMeterElement,
+        basePrice,
+        priceList
+      );
+
+      setPricesInDOM(pricePerSquareMetre, housingCard);
+      // const aTestDiv = document.createElement('div')
+      // aTestDiv.classList.add('mySimpleTest')
+      // adList.append(aTestDiv)
+
+      setTransportButton(housingCard, () => {
+        findTrips("Karenslyst alle 56", "Montebellobakken 3A");
+      });
+    } catch (error) {
+      console.error(error);
+      break;
     }
   }
+}
 
-  main()
+main();
 
-  mutationObserver.observe(adList, { childList: true })
+mutationObserver.observe(adList, { childList: true });
--- src/services/entur.js
+++ src/services/entur.js
@@ -1,52 +1,52 @@
-import createEnturService from '@entur/sdk'
-import axios from 'axios'
+import createEnturService from "@entur/sdk";
+import axios from "axios";
 
-import secondsToMinutes from '../helpers/secondsToMinutes'
-import getDateHoursMin from '../helpers/getDateHoursMin'
-
+import secondsToMinutes from "../helpers/secondsToMinutes";
+import getDateHoursMin from "../helpers/getDateHoursMin";
 
 const journeyPlannerService = createEnturService({
   clientName: "tanettrimas-finnprisperkvadratmeter",
 });
 
 export async function getTripSuggestions(address) {
-  const placeSuggestions = await journeyPlannerService.getFeatures(address)
-  return placeSuggestions.map(extractMetadata)
+  const placeSuggestions = await journeyPlannerService.getFeatures(address);
+  return placeSuggestions.map(extractMetadata);
 }
 
 function extractMetadata(place) {
-  const { geometry: { coordinates: [lng, lat] }, properties: {
-    name,
-    label, 
-    category,
-    street, 
-    housenumber,
-    source_id
-  }} = place
+  const {
+    geometry: {
+      coordinates: [lng, lat],
+    },
+    properties: { name, label, category, street, housenumber, source_id },
+  } = place;
   return {
     coordinates: {
       latitude: lat,
-      longitude: lng
+      longitude: lng,
     },
-    name, 
+    name,
     label,
-    category, 
+    category,
     street,
     housenumber,
-    place: source_id
-  }
+    place: source_id,
+  };
 }
 
 async function getBaseTripInformation(fromPlace, toPlace) {
   try {
-    const [fromData, toData] = await Promise.all([journeyPlannerService.getFeatures(fromPlace), journeyPlannerService.getFeatures(toPlace)])
+    const [fromData, toData] = await Promise.all([
+      journeyPlannerService.getFeatures(fromPlace),
+      journeyPlannerService.getFeatures(toPlace),
+    ]);
     // TODO: Replace this to make the user choose their prefered from/to from the autocomplete
     // Use localstorage
-    const from = fromData[0]
-    const to = toData[0]
-    
-    const [lngFrom, latFrom] = from.geometry.coordinates
-    const [lngTo, latTo] = to.geometry.coordinates
+    const from = fromData[0];
+    const to = toData[0];
+
+    const [lngFrom, latFrom] = from.geometry.coordinates;
+    const [lngTo, latTo] = to.geometry.coordinates;
     return {
       destinations: {
         from: {
@@ -55,32 +55,33 @@ async function getBaseTripInformation(fromPlace, toPlace) {
           category: from.properties.category,
           street: from.properties.street,
           housenumber: +from.properties.housenumber,
-          coordinates: [lngFrom, latFrom]
-        }, 
+          coordinates: [lngFrom, latFrom],
+        },
         to: {
           name: to.properties.name,
           label: to.properties.label,
           category: to.properties.category,
           street: to.properties.street,
           housenumber: +to.properties.housenumber,
-          coordinates: [lngTo, latTo]
-        }
+          coordinates: [lngTo, latTo],
+        },
       },
       baseTripMetadata: {
         to: {
           coordinates: { latitude: latTo, longitude: lngTo },
           place: to.properties.source_id,
-        }, 
+        },
         from: {
           coordinates: {
-            latitude: latFrom, 
-            longitude: lngFrom
+            latitude: latFrom,
+            longitude: lngFrom,
           },
           place: from.properties.source_id,
-        }
-      }
-    }
+        },
+      },
+    };
   } catch (error) {
+    console.error(error);
   }
 }
 
@@ -88,25 +89,34 @@ export default async function findTrips(from, to) {
   try {
     // Check localstorage here first
     const baseTripData = await getBaseTripInformation(from, to);
-    const [trips, non_transit] = await Promise.all([journeyPlannerService.getTripPatterns(baseTripData.baseTripMetadata), getNonTransit(baseTripData.baseTripMetadata)]) 
-    const nonTransitTrip = Object.keys(non_transit.tripPatterns).reduce((acc, key) => {
-      const current = non_transit.tripPatterns[key]
-      acc[key] = {
-        mode: key,
-        duration: secondsToMinutes(current.duration),
-        startTime: getDateHoursMin(current.startTime),
-        endTime: getDateHoursMin(current.endTime),
-        legs: current.legs,
-        ...(key === 'foot' ? {
-          distance: Math.floor(current.walkDistance)
-        } : {
-          distance: Math.floor(current.distance)
-        })
-      }
-      return acc
-    }, {})
+    const [trips, non_transit] = await Promise.all([
+      journeyPlannerService.getTripPatterns(baseTripData.baseTripMetadata),
+      getNonTransit(baseTripData.baseTripMetadata),
+    ]);
+    const nonTransitTrip = Object.keys(non_transit.tripPatterns).reduce(
+      (acc, key) => {
+        const current = non_transit.tripPatterns[key];
+        acc[key] = {
+          mode: key,
+          duration: secondsToMinutes(current.duration),
+          startTime: getDateHoursMin(current.startTime),
+          endTime: getDateHoursMin(current.endTime),
+          legs: current.legs,
+          ...(key === "foot"
+            ? {
+                distance: Math.floor(current.walkDistance),
+              }
+            : {
+                distance: Math.floor(current.distance),
+              }),
+        };
+        return acc;
+      },
+      {}
+    );
     const tripData = trips.map(({ legs, startTime, endTime, duration }) => {
-      const stages = legs.map(({ distance, mode, fromPlace, toPlace, line, duration }) => ({
+      const stages = legs.map(
+        ({ distance, mode, fromPlace, toPlace, line, duration }) => ({
           distance: Math.floor(distance),
           minutes: secondsToMinutes(duration),
           mode,
@@ -114,36 +124,39 @@ export default async function findTrips(from, to) {
             name: fromPlace.name,
             coords: [fromPlace.longitude, fromPlace.latitude],
             ...(fromPlace.quay && {
-              id: fromPlace.quay.stopPlace.id
-            })
+              id: fromPlace.quay.stopPlace.id,
+            }),
           },
           to: {
             name: toPlace.name,
             coords: [toPlace.longitude, toPlace.latitude],
             ...(toPlace.quay && {
-              id: toPlace.quay.stopPlace.id
-            })
+              id: toPlace.quay.stopPlace.id,
+            }),
           },
           ...(line && {
             lineName: line.name,
             lineNumber: line.publicCode,
             lineId: line.id,
-            ...(line.notices.length && line.notices)
-          })
-      }))
+            ...(line.notices.length && line.notices),
+          }),
+        })
+      );
       return {
         destinations: baseTripData.destinations,
         transit: {
           duration: secondsToMinutes(duration),
           legs: stages,
           startTime: getDateHoursMin(startTime),
-          endTime: getDateHoursMin(endTime)
+          endTime: getDateHoursMin(endTime),
         },
-        non_transit: nonTransitTrip
-      }
-    })
-    
-    return tripData.sort((a, b) => a.duration > b.duration)
+        non_transit: nonTransitTrip,
+      };
+    });
+
+    console.log("Trip data result", tripData);
+
+    return tripData.sort((a, b) => a.duration > b.duration);
   } catch (error) {
     console.error(error);
   }
@@ -162,7 +175,7 @@ async function getNonTransit({ from, to }) {
         walkSpeed: 1.3,
       },
     });
-    return response.data
+    return response.data;
   } catch (error) {
     console.error(error.response);
   }
