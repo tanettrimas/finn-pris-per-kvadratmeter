5f1a4709a487f9562dc2e8d32bb6ca978f46cdb5
Tanet Trimas <tanet@trimas.no>
2021-07-30 22:26:28 +0200

    Add dotenv dep for webstore upload

--- utils/release/updateVersionFiles.js
+++ utils/release/updateVersionFiles.js
@@ -1,36 +1,43 @@
-const fs = require('fs');
-const packageJSON = require('../../package.json')
-const manifestJSON = require('../../src/manifest.json')
+const fs = require("fs");
+require("dotenv").config();
+const packageJSON = require("../../package.json");
+const manifestJSON = require("../../src/manifest.json");
 
-
-function updatePackageJson({ key, value}) {
+function updatePackageJson({ key, value }) {
   return new Promise((resolve, reject) => {
-    packageJSON[key] = value
-    fs.writeFile('./package.json', JSON.stringify(packageJSON, null, 2), function(err) {
-      if (err) {
-        reject(new Error(err.message))
+    packageJSON[key] = value;
+    fs.writeFile(
+      "./package.json",
+      JSON.stringify(packageJSON, null, 2),
+      function (err) {
+        if (err) {
+          reject(new Error(err.message));
+        }
+        console.log("writing to package.json");
+        resolve(value);
       }
-      console.log('writing to package.json');
-      resolve(value)
-    });
-  })
+    );
+  });
 }
 
-function updateManifest({ key, value}) {
+function updateManifest({ key, value }) {
   return new Promise((resolve, reject) => {
-    manifestJSON[key] = value
-    fs.writeFile('./src/manifest.json', JSON.stringify(manifestJSON, null, 2), function(err) {
-      if (err) {
-        reject(new Error(err.message))
+    manifestJSON[key] = value;
+    fs.writeFile(
+      "./src/manifest.json",
+      JSON.stringify(manifestJSON, null, 2),
+      function (err) {
+        if (err) {
+          reject(new Error(err.message));
+        }
+        console.log("writing to manifest.json");
+        resolve(value);
       }
-      console.log('writing to manifest.json');
-      resolve(value)
-    });
-  })
+    );
+  });
 }
 
 module.exports = {
   updatePackageJson,
-  updateManifest
-}
-
+  updateManifest,
+};
